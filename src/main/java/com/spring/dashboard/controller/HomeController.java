5b0cac4503cc254ff60dddb2f4ef3435d597281e
Sahil Chopra <scho20@sapient.com>
2017-07-03 21:18:13 +0530

    --Added several html files

--- src/main/java/com/spring/dashboard/controller/HomeController.java
+++ src/main/java/com/spring/dashboard/controller/HomeController.java
@@ -3,17 +3,31 @@ package com.spring.dashboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class HomeController {
 
 	@RequestMapping("/home")
 	public String home( Model modal){
+		// enable logging
+		/*log.debug("In home Controller");*/
 		return "index";
 	}
 	
 	@RequestMapping("/addProject")
 	public String addProject( Model modal){
 		return "AddProject";
+	}
+	
+	@RequestMapping("/help")
+	public String help( Model modal){
+		return "Help";
+	}
+	
+	@RequestMapping("/settings")
+	public String settings( Model modal){
+		return "Settings";
 	}
 }
